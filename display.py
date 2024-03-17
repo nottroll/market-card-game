c36526de215bb62ce3588843e9089e32766f1618
nottroll <alsonlee97@gmail.com>
2024-03-17 21:58:18 +1100

    Implement core game functions

---
+++ display.py
@@ -0,0 +1,15 @@
+"""
+Alson Lee
+Date: 15/03/24
+
+The display module handles simple console output.
+"""
+
+MAX_DISPLAY_WIDTH = 60
+LPAD = 4
+
+def print_divider():
+    """
+    Prints a divider.
+    """
+    print('-' * MAX_DISPLAY_WIDTH, '\n')
